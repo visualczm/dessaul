a63c410b3e28d7c5a119e0ef3012e7ab28757554
Po <448443959@qq.com>
2019-12-10 16:33:49 +0800

    修复无法删除文件问题

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -70,29 +70,29 @@ class ProductController extends AdminController
 
     /**
      * Make a form builder.
-     *  $grid->column('model','产品型号');
-    $grid->column('name','产品名称');
-    $grid->column('images','产品图片');
-    $grid->column('desc', '产品说明');
-    $grid->column('explain','说明文件');
-    $grid->column('downfile','下载文件');
-    $grid->column('downdriver','下载驱动');
-    $grid->column('created_at', '建立日期');
-    $grid->column('updated_at', '更新日期');
      * @return Form
      */
     protected function form()
     {
         $form = new Form(new Product);
-        $form->select('parent_id',"品牌")->options(NavCategory::pluck("name","id"))->load('category_id','/api/getcategory');
+
+
+        $form->select('parent_id',"品牌")->options(NavCategory::pluck("name","id"))
+            ->load('category_id','/api/getcategory');
         $form->select('category_id','产品类别');
+
+
         $form->text('name', __('产品名称'));
         $form->text('model','产品型号');
-        $form->image('images','图片');
-        $form->file('explain', __('说明文件'));
-        $form->file('downfile', __('下载文件'));
-        $form->file('downdriver', __('下载驱动'));
+        $form->image('images','图片')->removable();
+        $form->file('explain', __('说明文件'))->removable();
+        $form->file('downfile', __('下载文件'))->removable();
+        $form->file('downdriver', __('下载驱动'))->removable();
         $form->textarea('desc', __('产品说明'));
+
+
+
+
 
         return $form;
     }
