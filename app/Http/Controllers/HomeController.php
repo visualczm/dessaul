ab604dc1b4c8cbd05eb8f5a912881b204116bd65
Po <448443959@qq.com>
2020-01-05 12:33:05 +0800

    第一版上线

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,9 +13,9 @@ class HomeController extends Controller
      $nav= new \App\Models\NavCategory();//获取产品列表
      $imports=  $nav->getNavCategory(1);//进口列表
 
-     $about=\App\models\About::select('mission')->findOrFail(1);
+     $about=\App\Models\About::select('mission')->findOrFail(1);
 
-     $product=\App\models\Product::where('push','=',1)->get();
+     $product=\App\Models\Product::where('push','=',1)->get();
 
 
     return view('index',compact('imports','about','product'));
